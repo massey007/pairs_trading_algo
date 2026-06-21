ab7333f42c1a5a61502c6bdc45a33d95f8dbb697
Masimba007 <masimba@hudsonthames.org>
2026-06-21 18:30:51 +0200

    Added yield curve data loader

---
+++ us_yc_market_data.py
@@ -0,0 +1,126 @@
+# This code will pull the entire yield curve from the FRED API and save it to a CSV file.
+# It will also print the yield curve to the console.
+
+from tracemalloc import start
+
+import matplotlib.pyplot as plt
+import yfinance as yf
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import datetime
+import pandas_datareader as pdr
+from scipy.cluster.hierarchy import linkage, dendrogram
+from fredapi import Fred
+fred = Fred(api_key='17a30f1e933d9c9de2afc4bcd7a5fbcb')
+
+class us_yield_curve_market_data:
+    def __init__(self, start_date, end_date):
+
+        self.start_date = start_date
+        self.end_date = end_date
+        self.repo = None
+        self.one_month = None
+        self.three_month = None
+        self.six_month = None
+        self.one_year = None
+        self.two_year = None
+        self.three_year = None
+        self.five_year = None
+        self.ten_year = None
+        self.twenty_year = None
+        self.thirty_year = None
+        self.data = None
+
+    def us_yield_curve(self):
+
+        # Fetch the yield curve data from FRED
+        self.repo = fred.get_series('DFF', self.start_date, self.end_date)
+        self.repo = pd.DataFrame(self.repo)
+        self.repo.rename(columns={0:'Repo_Rate_US'}, inplace=True)
+
+        self.one_month = fred.get_series('DGS1MO', self.start_date, self.end_date)
+        self.one_month = pd.DataFrame(self.one_month)
+        self.one_month.rename(columns={0:'One_Month_US'}, inplace=True)
+
+        self.three_month = fred.get_series('DGS3MO', self.start_date, self.end_date)
+        self.three_month = pd.DataFrame(self.three_month)
+        self.three_month.rename(columns={0:'Three_Month_US'}, inplace=True)
+
+        self.six_month = fred.get_series('DGS6MO', self.start_date, self.end_date)
+        self.six_month = pd.DataFrame(self.six_month)
+        self.six_month.rename(columns={0:'Six_Month_US'}, inplace=True)
+
+        self.one_year = fred.get_series('DGS1', self.start_date, self.end_date)
+        self.one_year = pd.DataFrame(self.one_year)
+        self.one_year.rename(columns={0:'One_Year_US'}, inplace=True)
+
+        self.two_year = fred.get_series('DGS2', self.start_date, self.end_date)
+        self.two_year = pd.DataFrame(self.two_year)
+        self.two_year.rename(columns={0:'Two_Year_US'}, inplace=True)
+
+        self.five_year = fred.get_series('DGS5', self.start_date, self.end_date)
+        self.five_year = pd.DataFrame(self.five_year)
+        self.five_year.rename(columns={0:'Five_Year_US'}, inplace=True)
+
+        self.ten_year = fred.get_series('DGS10', self.start_date, self.end_date)
+        self.ten_year = pd.DataFrame(self.ten_year)
+        self.ten_year.rename(columns={0:'Ten_Year_US'}, inplace=True)
+
+        self.twenty_year = fred.get_series('DGS20', self.start_date, self.end_date)
+        self.twenty_year = pd.DataFrame(self.twenty_year)
+        self.twenty_year.rename(columns={0:'Twenty_Year_US'}, inplace=True)
+
+        self.thirty_year = fred.get_series('DGS30', self.start_date, self.end_date)
+        self.thirty_year = pd.DataFrame(self.thirty_year)
+        self.thirty_year.rename(columns={0:'Thirty_Year_US'}, inplace=True)
+
+
+    def us_market_data(self, ticker):
+        self.ticker = ticker
+        ticker = ['^GSPC', '^VIX', 'GC=F', 'BZ=F']
+        self.data = yf.download(self.ticker, start=self.start_date, end=self.end_date)
+
+    def merge_data(self):
+        if self.data is not None:
+            self.data = pd.DataFrame({
+                'sp500': self.data['Close']['^GSPC'].squeeze(),
+                'vix': self.data['Close']['^VIX'].squeeze(),
+                'gold': self.data['Close']['GC=F'].squeeze(),
+                'oil': self.data['Close']['BZ=F'].squeeze(),
+                'Repo_Rate_US': self.repo['Repo_Rate_US'].squeeze(),
+                'One_Month_US': self.one_month['One_Month_US'].squeeze(),
+                'Three_Month_US': self.three_month['Three_Month_US'].squeeze(),
+                'Six_Month_US': self.six_month['Six_Month_US'].squeeze(),
+                'One_Year_US': self.one_year['One_Year_US'].squeeze(),
+                'Two_Year_US': self.two_year['Two_Year_US'].squeeze(),
+                'Five_Year_US': self.five_year['Five_Year_US'].squeeze(),
+                'Ten_Year_US': self.ten_year['Ten_Year_US'].squeeze(),
+                'Twenty_Year_US': self.twenty_year['Twenty_Year_US'].squeeze(),
+                'Thirty_Year_US': self.thirty_year['Thirty_Year_US'].squeeze()
+            })
+            self.data.dropna(inplace=True)
+        else:
+            print("No data to merge. Please fetch data first.")
+
+    def plot_yield_curve(self):
+        if self.data is not None:
+            plt.figure(figsize=(12, 6))
+            plt.plot(self.data.index, self.data['Repo_Rate_US'], label='Repo Rate')
+            plt.plot(self.data.index, self.data['One_Month_US'], label='1 Month')
+            plt.plot(self.data.index, self.data['Three_Month_US'], label='3 Month')
+            plt.plot(self.data.index, self.data['Six_Month_US'], label='6 Month')
+            plt.plot(self.data.index, self.data['One_Year_US'], label='1 Year')
+            plt.plot(self.data.index, self.data['Two_Year_US'], label='2 Year')
+            plt.plot(self.data.index, self.data['Five_Year_US'], label='5 Year')
+            plt.plot(self.data.index, self.data['Ten_Year_US'], label='10 Year')
+            plt.plot(self.data.index, self.data['Twenty_Year_US'], label='20 Year')
+            plt.plot(self.data.index, self.data['Thirty_Year_US'], label='30 Year')
+            plt.title('US Yield Curve Over Time')
+            plt.xlabel('Date')
+            plt.ylabel('Yield (%)')
+            plt.legend()
+            plt.grid()
+            plt.show()
+        else:
+            print("No data to plot. Please fetch and merge data first.")
